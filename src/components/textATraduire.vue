97f4fdbbae331def8b2a0e8d8f0bf6cbc4c73be5
Julie.Canevet <julie.canevet@hotmail.fr>
2017-08-31 09:53:36 +0200

    first commit

---
+++ src/components/textATraduire.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="texte">
+  <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
